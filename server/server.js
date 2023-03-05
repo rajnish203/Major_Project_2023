6fdd8d645adb10d31e2059a6ccd65fa885500f73
rajnish203 <rajnishdivakar618@gmail.com>
2023-03-05 15:50:48 +0530

    some updation required

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
+    message: "Let's Explore"
   })
 })
 
@@ -44,4 +44,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () => console.log('AI server started on https://g1ai.onrender.com/'))
